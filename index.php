669a8cab46b5d5fa7923b7046ba871698a2850d8
Jonah <jonah.watts@gmail.com>
2022-03-08 14:44:28 -0700

    POO

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <h1>Welcome to my website</h1>
+
+<?php 
+    $x = "Hello world!";
+    $y = 'Hello world!';
+
+    echo $x;
+    echo "<br>"; 
+    echo $y;
+?>
+
+</body>
+
+</html>
